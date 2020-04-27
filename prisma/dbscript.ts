8af2e17d474f6cb35ff510f25ff1fd4cc298086d
roblotter <roblotter1@gmail.com>
2020-04-27 18:36:35 +0300

    update

--- prisma/dbscript.ts
+++ prisma/dbscript.ts
@@ -1,50 +1,50 @@
-const { PrismaClient } = require("@prisma/client");
+// const { PrismaClient } = require("@prisma/client");
 
-const ordersjson = require("./orders.json");
+// const ordersjson = require("./orders.json");
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-// A `main` function so that you can use async/await
-async function main() {
-  for await (let order of ordersjson) {
-    // console.log(order);
-    delete order.OrderId;
-    if (order.ShipPostalCode) {
-      order.ShipPostalCode = `${order.ShipPostalCode}`;
-    }
-    if (order.IsValid) {
-      order.IsValid =
-        typeof order.IsValid === "string"
-          ? order.IsValid.toLowerCase() === "true"
-            ? true
-            : false
-          : false;
-    }
+// // A `main` function so that you can use async/await
+// async function main() {
+//   for await (let order of ordersjson) {
+//     // console.log(order);
+//     delete order.OrderId;
+//     if (order.ShipPostalCode) {
+//       order.ShipPostalCode = `${order.ShipPostalCode}`;
+//     }
+//     if (order.IsValid) {
+//       order.IsValid =
+//         typeof order.IsValid === "string"
+//           ? order.IsValid.toLowerCase() === "true"
+//             ? true
+//             : false
+//           : false;
+//     }
 
-    if (order.ShipRegion === null) {
-      delete order.ShipRegion;
-    }
+//     if (order.ShipRegion === null) {
+//       delete order.ShipRegion;
+//     }
 
-    ["OrderDate", "RequiredDate", "ShippedDate", "LastUpdatedTime"].forEach(
-      (property) => {
-        if (order[property]) {
-          order[property] = new Date(order[property]);
-        }
-      }
-    );
+//     ["OrderDate", "RequiredDate", "ShippedDate", "LastUpdatedTime"].forEach(
+//       (property) => {
+//         if (order[property]) {
+//           order[property] = new Date(order[property]);
+//         }
+//       }
+//     );
 
-    let createdOrder = await prisma.order.create({ data: order });
+//     let createdOrder = await prisma.order.create({ data: order });
 
-    // console.log(createdOrder);
-  }
-}
+//     // console.log(createdOrder);
+//   }
+// }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.disconnect();
+//   });
 
-module.exports = main;
+// module.exports = main;
